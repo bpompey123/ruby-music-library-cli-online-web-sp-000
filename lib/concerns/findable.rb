4427ba6d14d5ab548f13e0992e7b27673735346e
Brian Pompey <brianthescholar@gmail.com>
2019-10-23 01:06:28 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,4 +1,14 @@
 module Concerns::ModuleName
+  def find_by_name(name)
+    self.all.find{|song_name| song_name.name == name}
+  end
 
+  def find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else
+      self.create(name)
+    end
+  end
 
 end
