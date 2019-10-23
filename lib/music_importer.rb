506944143a9f654e8d6e07fdca953fbac6b2fd87
Brian Pompey <brianthescholar@gmail.com>
2019-10-23 01:18:48 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -6,7 +6,13 @@ class MusicImporter
   end
 
   def files
+    Dir.entries(@path).select! {|entry| entry.end_with?(".mp3")}
+      end
 
+  def import
+    self.files.each do |filename|
+      Song.create_from_filename(filename)
+    end
   end
 
 
